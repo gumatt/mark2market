336684479ddb76b775d3df3ff2dd6b89c00c9f26
Matt Walker <gumatt@gmail.com>
2015-09-12 20:43:30 -0700

    checking deploy without specifying address and port

--- mark2market.py
+++ mark2market.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,5 +9,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)

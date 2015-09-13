2328d612b5635dab9601a4a81a1bf55b4c85dc2f
Matt Walker <gumatt@gmail.com>
2015-09-12 22:11:52 -0700

    added mongodb support to app foundation

--- mark2market.py
+++ mark2market.py
@@ -1,9 +1,31 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template, request, redirect
+
+from pymongo import MongoClient
+
+MONGO_URL = os.environ.get('MONGOHQ_URL')
+client = MongoClient(MONGO_URL)
+
+# Specify the database
+db = client.testapp
+collection = db.accounts
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    accounts = collection.find()
+    return render_template('index.html', accounts=accounts)
+
+
+@app.route('/', methods=['POST'])
+def post():
+    account = {"name": request.form['name'], "description": request.form['description']}
+    account_id = collection.insert(account)
+    return redirect('/')
+
 def hello_world():
     return 'Hello World!'
 
